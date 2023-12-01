d7ff883a925b6ba879afdc915d2b10719cf3dc3f
ThomasLedlie <87582683+ThomasLedlie@users.noreply.github.com>
2023-12-01 11:20:45 +0000

    Update sensor_shield.ino

--- sensor_shield/sensor_shield.ino
+++ sensor_shield/sensor_shield.ino
@@ -10,10 +10,10 @@
 int chk;
 int sensorValue = 0; //variable to store the value coming from the sensor
 void setup() { // put your setup code here, to run once:
- Serial.begin(9600); // USB baud rate
+    Serial.begin(9600); // USB baud rate
 }
 void loop() { //put your main code here, to run repeatedly:
- sensorValue = analogRead(TSLPIN); //read light value from sensor
- Serial.println(sensorValue); //prints value from the sensor to serial monitor
- delay(1000); //Delay 1000ms = 1 sec
+    sensorValue = analogRead(TSLPIN); //read light value from sensor
+    Serial.println(sensorValue); //prints value from the sensor to serial monitor
+    delay(1000); //Delay 1000ms = 1 sec
 }

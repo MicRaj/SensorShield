ed1ce9f90e1afc23125938154b993423dea46663
ThomasLedlie <87582683+ThomasLedlie@users.noreply.github.com>
2023-12-01 11:33:36 +0000

    Update sensor_shield.ino

--- sensor_shield/sensor_shield.ino
+++ sensor_shield/sensor_shield.ino
@@ -4,16 +4,24 @@
 // TL2060 | MR2207 //
 // *************** //
 
+// Inclusions
+#include <SoftWire.h>
+#include <HIH61xx.h>
+#include <AsyncDelay.h>
+
 //Constants
 #define TSLPIN A1 //tell the program what pin the TSL257 is attached to
-//Variables
+
+// Variables
 int chk;
 int sensorValue = 0; //variable to store the value coming from the sensor
+
 void setup() { // put your setup code here, to run once:
     Serial.begin(9600); // USB baud rate
 }
+
 void loop() { //put your main code here, to run repeatedly:
     sensorValue = analogRead(TSLPIN); //read light value from sensor
     Serial.println(sensorValue); //prints value from the sensor to serial monitor
-    delay(1000); //Delay 1000ms = 1 sec
+    delay(100); //Delay 1000ms = 1 sec
 }

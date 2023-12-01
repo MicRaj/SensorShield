c577862000cb014907a457aefffeea5ac95b5a38
ThomasLedlie <87582683+ThomasLedlie@users.noreply.github.com>
2023-12-01 12:01:01 +0000

    Update sensor_shield.ino

--- sensor_shield/sensor_shield.ino
+++ sensor_shield/sensor_shield.ino
@@ -15,7 +15,8 @@
 // Variables
 int chk;
 int sensorValue = 0; //variable to store the value coming from the sensor
-
+SoftWire sw(SDA, SCL);
+ 
 void setup() { // put your setup code here, to run once:
   #if F_CPU >= 12000000UL
     Serial.begin(9600);
@@ -42,7 +43,32 @@ void setup() { // put your setup code here, to run once:
     samplingInterval.start(1000, AsyncDelay::MILLIS);
 }
 
+bool printed = true; 
+
 void loop() { //put your main code here, to run repeatedly:
+    if (samplingInterval.isExpired() && !hih.isSampling()) {
+      hih.start();
+      printed = false;
+      samplingInterval.repeat();
+      Serial.println("Sampling started (using SoftWire library)");
+    }
+
+    hih.process();
+
+
+    if (hih.isFinished() && !printed) {
+      printed = true;
+    // Print saved values
+      Serial.print("RH: ");
+      Serial.print(hih.getRelHumidity() / 100.0);
+      Serial.println(" %");
+      Serial.print("Ambient: ");
+      Serial.print(hih.getAmbientTemp() / 100.0);
+      Serial.println(" deg C");
+      Serial.print("Status: ");
+      Serial.println(hih.getStatus());
+}
+
     sensorValue = analogRead(TSLPIN); //read light value from sensor
     Serial.println(sensorValue); //prints value from the sensor to serial monitor
     delay(100); //Delay 1000ms = 1 sec

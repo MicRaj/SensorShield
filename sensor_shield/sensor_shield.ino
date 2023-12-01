ca5b06fe6607b94b4251254a0234b33caaf43b23
ThomasLedlie <87582683+ThomasLedlie@users.noreply.github.com>
2023-12-01 11:09:46 +0000

    test
    test

--- sensor_shield/sensor_shield.ino
+++ sensor_shield/sensor_shield.ino
@@ -13,3 +13,22 @@ void loop() { //put your main code here, to run repeatedly:
  Serial.println(sensorValue); //prints value from the sensor to serial monitor
  delay(1000); //Delay 1000ms = 1 sec
 }
+
+//Constants
+#define TSLPIN A1 //tell the program what pin the TSL257 is attached to
+//Variables
+int chk;
+int sensorValue = 0; //variable to store the value coming from the sensor
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); // USB baud rate
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  sensorValue = analogRead(TSLPIN); //read light value from sensor
+  Serial.println(sensorValue); //prints value from the sensor to serial monitor
+  delay(1000); //Delay 1000ms = 1 sec
+}
